1e07a80027a67cb0a1c82452c8db6ec09165e14b
Zeroh729 <joyceplayingcp@yahoo.com>
2016-10-29 06:01:43 +0800

    pitchable web app

---
+++ Web/public/firebase.js
@@ -0,0 +1,11 @@
+// import firebase from 'firebase';
+// var firebaseConfig = {
+// 	    apiKey: "AIzaSyCAByGQq3JGgOioLLM2WGKnnLRVv829WwM",
+// 	    authDomain: "blue-booth-fairy.firebaseapp.com",
+// 	    databaseURL: "https://blue-booth-fairy.firebaseio.com",
+// 	    storageBucket: "blue-booth-fairy.appspot.com",
+// 	    messagingSenderId: "589111553844"
+// 	};
+
+// var FbApp = firebase.initializeApp(firebaseConfig);
+// module.exports.FBApp = FbApp.database(); 
